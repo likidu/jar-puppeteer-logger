771cfec1de8c8618ecb0038a92a91cc4ea768c39
Liya Du <liydu@microsoft.com>
2022-12-13 23:48:03 -0800

    WIP 1213: Login Chiphell works now

--- src/login.ts
+++ src/login.ts
@@ -11,6 +11,12 @@ type Response = {
   cookie: Protocol.Network.Cookie[]
 }
 
+async function delay(ms) {
+  return new Promise(resolve => {
+    setTimeout(resolve, ms)
+  })
+}
+
 async function login(request: LoginRequest): Promise<Response | string> {
   const { username, password, forum } = request
   console.log(`[puppeteer.app.login] DEBUG: Login: ${forum}`)
@@ -19,6 +25,7 @@ async function login(request: LoginRequest): Promise<Response | string> {
   let url,
     usernameSelector,
     passwordSelector,
+    verifySelector,
     submitSelecor,
     cookieCheck: string
   let hasCaptcha = false
@@ -39,7 +46,8 @@ async function login(request: LoginRequest): Promise<Response | string> {
       // Selectors
       usernameSelector = 'input[name=username]'
       passwordSelector = 'input[name=password]'
-      submitSelecor = '#codeVerifyButton'
+      verifySelector = '#codeVerifyButton'
+      submitSelecor = '#btn_login'
 
     default:
       break
@@ -68,7 +76,7 @@ async function login(request: LoginRequest): Promise<Response | string> {
   const page = await browser.newPage()
 
   // Emluate iPhone X
-  const device = KnownDevices['iPhone X']
+  const device = KnownDevices['iPhone 12']
   await page.emulate(device)
 
   // Pause all medias
@@ -86,37 +94,37 @@ async function login(request: LoginRequest): Promise<Response | string> {
 
   try {
     await page.goto(url, {
-      waitUntil: ['domcontentloaded', 'load', 'networkidle0'],
+      waitUntil: ['domcontentloaded', 'load', 'networkidle2'],
     })
 
-    // Login
-    await page.type(usernameSelector, username, { delay: 100 })
-    await page.type(passwordSelector, password, { delay: 100 })
-    await page.click(submitSelecor)
+    // Enter credentials
+    await page.type(usernameSelector, username, { delay: 20 })
+    await page.type(passwordSelector, password, { delay: 20 })
 
     if (hasCaptcha) {
-      const frame = await page.waitForFrame(async frame => {
-        return (
-          frame.$('iframe[id=tcaptcha_iframe] #tcaptcha_drag_button') !== null
-        )
-      })
+      await page.click(verifySelector)
 
-      // await page.waitForSelector('iframe[id=tcaptcha_iframe]')
-      // console.log('[puppeteer.app.login] DEBUG: iFrame loaded.')
+      // const frame = await page.waitForFrame(async frame => {
+      //   return frame.$('#tcaptcha_iframe') !== null
+      // })
+      const iFrameSelector = '#tcaptcha_iframe'
+      const bgImageSelector = 'img[id="slideBg"]'
+      const blockImageSelector = 'img[id="slideBlock"]'
 
-      // const frameHandle = await page.$('iframe[id=tcaptcha_iframe]')
-      // const frame = await frameHandle.contentFrame()
+      await page.waitForSelector(iFrameSelector)
+      const frameHandle = await page.$(iFrameSelector)
+      const frame = await frameHandle.contentFrame()
+
+      await frame.waitForSelector('img[id="slideBg"]')
+      // Wait for the image loaded
+      await delay(3000)
 
       const offset = await frame.evaluate(() => {
-        console.log(`[puppeteer.app.login] DEBUG: In offset now.`)
-
         // Get captcha image sizes
-        const bg = document.querySelector<HTMLImageElement>('img[id=slideBg]')
+        const bg = document.querySelector<HTMLImageElement>('img[id="slideBg"]')
 
         const w = bg.naturalWidth
         const h = bg.naturalHeight
-
-        console.log(`[puppeteer.app.login] DEBUG: Captcha size: ${w} x ${h}`)
 
         // Draw captcha image in Canvas to get data of every pixel
         const cvs = document.createElement('canvas')
@@ -125,9 +133,9 @@ async function login(request: LoginRequest): Promise<Response | string> {
         const ctx = cvs.getContext('2d')
         ctx.drawImage(bg, 0, 0)
 
-        const block = document.querySelector(
-          'img[id=slideBlock]'
-        ) as HTMLImageElement
+        const block = document.querySelector<HTMLImageElement>(
+          'img[id="slideBlock"]'
+        )
         const y = parseInt(block.style.top) * 2 + 40
         let lastWhite = -1
         for (let x = w / 2; x < w; x++) {
@@ -151,45 +159,40 @@ async function login(request: LoginRequest): Promise<Response | string> {
         }
       })
 
-      console.log(`[puppeteer.app.login] DEBUG: Offset: ${offset}`)
+      console.log(`[puppeteer.app.login] DEBUG: Offset = ${offset}`)
 
       // Get the slide control
       const slideHandle = await frame.$('#tcaptcha_drag_thumb')
       const slide = await slideHandle.boundingBox()
 
+      const x = slide.x + slide.width / 2
+      const y = slide.y + slide.height / 2
+
       // Move mouse to the center of the slide control and press
-      await page.mouse.move(
-        slide.x + slide.width / 2,
-        slide.y + slide.height / 2
-      )
+      await page.mouse.move(x, y)
       await page.mouse.down()
 
-      //
-      let current = 0
-      while (Math.abs(offset - current) > 5) {
-        // Move 2-10 pixels each time
-        const distance = Math.round(Math.random() * 8) + 2
-        current += distance
-        await page.mouse.move(slide.x + distance, slide.y + slide.height / 2)
-        console.log(
-          `[puppeteer.app.login] DEBUG: Moving: ${current} / ${offset}`
-        )
-      }
-
       // Release mouse
+      await page.mouse.move(x + offset, y, { steps: 30 })
       await page.mouse.up()
+
+      await delay(1000)
     }
+
+    // Login
+    await page.click(submitSelecor)
     await page.waitForNavigation()
 
     // Get cookies
     const cookie = { cookie: await page.cookies() }
-    await browser.close()
     return cookie
   } catch (error) {
     console.log(
       `[puppeteer.app.login] ERROR: Login failed URL: ${url} Error:${error}`
     )
     return 'Login error!'
+  } finally {
+    await browser.close()
   }
 }
 
